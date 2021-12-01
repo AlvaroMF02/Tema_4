9f4ee2bb454a556477700a77e8b30fa7c4146b66
alvaro <alvaromarflo@gmail.com>
2021-12-01 13:33:12 +0100

    cafetera

--- src/main/java/ejer8cCafetera/Cafetera.java
+++ src/main/java/ejer8cCafetera/Cafetera.java
@@ -5,5 +5,43 @@ package ejer8cCafetera;
  * @author alvaro
  */
 public class Cafetera {
+    private int capacidadMaxima;
+    private int cantidadActual;
+
+    
+    //CONSTRUCTOR
+    public Cafetera(int capacidadMaxima, int cantidadActual) {
+        this.capacidadMaxima=1000;
+        this.cantidadActual=0;
+    }
+
+  
+    
+    public int getCapacidadMaxima() {
+        return capacidadMaxima;
+    }
+
+    public void setCapacidadMaxima(int capacidadMaxima) {
+        this.capacidadMaxima = capacidadMaxima;
+    }
+
+    public int getCantidadActual() {
+        return cantidadActual;
+    }
+
+    public void setCantidadActual(int cantidadActual) {
+        this.cantidadActual = cantidadActual;
+    }
+    
+    
+    public int llenarCafetera(int capacidadMaxima){
+        capacidadMaxima=1000;
+     return capacidadMaxima;
+    }
+    
+    public double servirTaza(double cantidadAServir){
+        
+        this.cantidadActual-=cantidadAServir;
+    }
     
 }
