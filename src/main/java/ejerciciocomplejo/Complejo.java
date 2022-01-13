d3d040f407d662ac3eb2fe9217041e5dd07c7c84
alvaro <alvaromarflo@gmail.com>
2022-01-13 12:23:35 +0100

    ejercicio complejo

---
+++ src/main/java/ejerciciocomplejo/Complejo.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejer1CComplejo;
+
+/**
+ *
+ * @author Álvaro
+ */
+public class Complejo {
+    //el numero complejo (z) tiene parte real y parte imagniaria z = (a, b)
+    private double aReal;        // a=Re(z)
+    private double bImaginaria;  //b=Im(z)
+    
+    //-------------CONSTRUCTORES-------------
+
+    public Complejo(double aReal, double bImaginaria) {
+        this.aReal = aReal;
+        this.bImaginaria = bImaginaria;
+    }
+    
+    public Complejo(double aReal, double bImaginaria, String algo) {
+        this.aReal = 49;
+        this.bImaginaria = 56;
+        algo = "no se que hago";
+    }
+    
+
+    //-------------GET/SET-------------
+
+    public double getaReal() {
+        return aReal;
+    }
+
+    public void setaReal(double aReal) {
+        this.aReal = aReal;
+    }
+
+    public double getbImaginaria() {
+        return bImaginaria;
+    }
+
+    public void setbImaginaria(double bImaginaria) {
+        this.bImaginaria = bImaginaria;
+    }
+    
+    //-------------TO-STRING-------------
+
+    @Override
+    public String toString() {
+        return "Complejo" + "(a=" + aReal + ", b=" + bImaginaria+")";
+    }
+    
+    //-------------METODOS-------------
+    //https://www.problemasyecuaciones.com/complejos/sumar-restar-multiplicar-dividir-numeros-complejos-imaginarios-ejemplos-formulas.html
+    
+//    private static Complejo suma(Complejo num1, Complejo num2){
+//        double resultado;
+//       resultado=(num1.aReal+num2.aReal , num1.bImaginaria+num2.bImaginaria);
+//       
+//       return resultado;
+//    }
+//    
+//    private static Complejo resta(Complejo num1, Complejo num2){
+//        double resultado;
+//        resultado=(num1.aReal-num2.aReal , num1.bImaginaria-num2.bImaginaria);
+//        
+//        return resultado;
+//    }
+}
---
+++ src/main/java/ejerciciocomplejo/prueba.java
@@ -0,0 +1,15 @@
+package ejer1CComplejo;
+
+/**
+ *
+ * @author Álvaro
+ */
+public class prueba {
+    public static void main(String[] args) {
+        
+        Complejo num1 = new Complejo(12,23);
+        Complejo num2 = new Complejo(5,13);
+        
+        
+    }
+}
